6becc8dd8ad460e0f7955dd11970ff5b5da4edd9
timetravel-1010 <timetravel_1010@protonmail.com>
2023-12-12 08:03:40 -0500

    Add solution to day 1 part 2 problem in Go

--- 2023/go/day-1/main.go
+++ 2023/go/day-1/main.go
@@ -9,37 +9,107 @@ import (
 	"strings"
 )
 
-var values = map[string]int{
-	"zero":  0,
-	"one":   1,
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
-}
+var (
+	values = map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+	numbers = []string{
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+)
 
 func getCalibrationValue(line string) int {
-	chars := strings.Split(line, "")
+
+	// search by word
+	firstDigitIdx := -1
+	secondDigitIdx := -1
 	firstDigit := -1
 	secondDigit := -1
 
-	for _, c := range chars {
+	for _, num := range numbers {
+		if i := strings.Index(line, num); i != -1 {
+			if firstDigitIdx != -1 {
+				if i < firstDigitIdx {
+					if firstDigitIdx > secondDigitIdx {
+						secondDigitIdx = firstDigitIdx 
+						secondDigit = (firstDigit / 10)
+					}
+					firstDigitIdx = i
+					firstDigit = (values[num] * 10)
+				} else if i > secondDigitIdx {
+					secondDigitIdx = i 
+					secondDigit = values[num]
+				}
+			} else {
+				firstDigitIdx = i
+				firstDigit = (values[num] * 10)
+			}
+		}
+
+		if i := strings.LastIndex(line, num); i != -1 {
+			if firstDigitIdx != -1 {
+				if i < firstDigitIdx {
+					if firstDigitIdx > secondDigitIdx {
+						secondDigitIdx = firstDigitIdx 
+						secondDigit = (firstDigit / 10)
+					}
+					firstDigitIdx = i
+					firstDigit = (values[num] * 10)
+				} else if i > secondDigitIdx {
+					secondDigitIdx = i 
+					secondDigit = values[num]
+				}
+			} else {
+				firstDigitIdx = i
+				firstDigit = (values[num] * 10)
+			}
+		}
+	}
+
+	// search by character
+	chars := strings.Split(line, "")
+
+	for i, c := range chars {
 		num, err := strconv.Atoi(c)
 		if err == nil {
 			if firstDigit == -1 {
 				firstDigit = num * 10
 			} else {
-				secondDigit = num
+				if i < firstDigitIdx {
+					if firstDigitIdx > secondDigitIdx {
+						secondDigitIdx = firstDigitIdx 
+						secondDigit = (firstDigit / 10)
+					}
+					firstDigitIdx = i
+					firstDigit = (num * 10)
+				} else if i > secondDigitIdx {
+					secondDigitIdx = i
+					secondDigit = num
+				}
 			}
 		}
 	}
 
 	if secondDigit == -1 {
-		return firstDigit + firstDigit/10
+		return firstDigit + (firstDigit/10)
 	}
 	return firstDigit + secondDigit
 }
@@ -55,26 +125,24 @@ func readFile(fileName string) (*os.File, error) {
 func getSum(file *os.File) int {
 	scanner := bufio.NewScanner(file)
 	sum := 0
+	lines := 0
 	for scanner.Scan() {
 		sum += getCalibrationValue(scanner.Text())
+		lines++
 	}
+	fmt.Println(lines, "lines read.")
 	return sum
 }
 
 func main() {
+	
 	file, err := readFile("input.txt")
 	if err != nil {
 		panic("an error has occurred!")
 	}
 	fmt.Println(getSum(file))
-
-	cadena := "Hola,¿comoestas?"
-	palabra := "estas"
-
-	posicion := strings.Index(cadena, palabra)
-	if posicion != -1 {
-		fmt.Println("La palabra", palabra, "se encuentra en la posición", posicion, "en la cadena.")
-	} else {
-		fmt.Println("La palabra", palabra, "no se encuentra en la cadena.")
-	}
+	defer file.Close()
+	//fmt.Println(getCalibrationValue("vmzcrhtdvnm6fivepkbhcxj"))
+	//fmt.Println(getCalibrationValue("6t"))
+	// fmt.Println(getCalibrationValue("otwone3one3one"))
 }
